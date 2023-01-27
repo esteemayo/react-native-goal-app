322b4b221c6a0aa47b9d170981eda91cda25deb2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-27 22:04:34 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title='Change Text' onPress={() => setOutputText('The text changed!')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
